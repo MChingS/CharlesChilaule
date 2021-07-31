1e1be8e8b0956a62e1ec5ca2b02d9305b06e23b4
mchingis <53611127+MChingS@users.noreply.github.com>
2021-07-31 10:28:52 +0200

    collection

--- Collection/src/com/company/Main.java
+++ Collection/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         LinkedHashSet<studentList> list = new LinkedHashSet<studentList>();
 
+        //adding student to the linkedhashset
         studentList stud1 = new studentList(1,"charles","chilaule","a+");
         studentList stud2= new studentList(2,"bongani","shembe","a");
         studentList stud3 = new studentList(3,"mongeni","zwane","d");
@@ -16,6 +17,16 @@ public class Main {
         list.add(stud2);
         list.add(stud3);
         list.add(stud4);
+
+        //displaying the student
+        for(studentList temp:list)
+        {
+            System.out.println(temp.ID+" "+temp.fname+" "+temp.lname+" "+temp.grade);
+        }
+        list.remove(stud2);
+        System.out.println("");
+        System.out.println("_______After removing student________");
+        //display after removing student
         for(studentList temp:list)
         {
             System.out.println(temp.ID+" "+temp.fname+" "+temp.lname+" "+temp.grade);
