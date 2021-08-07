65f8f7cfd4a97397f774c8fbedce51e9f0307bd7
mchingis <53611127+MChingS@users.noreply.github.com>
2021-08-07 11:08:31 +0200

    student

---
+++ SchoolProject/src/com/company/StudentRegistration.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class StudentRegistration {
+
+    public void studentDetails()
+    {
+        System.out.println("Enter your name");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
