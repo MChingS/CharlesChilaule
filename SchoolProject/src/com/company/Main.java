83530afe4798f48358ef55335dd00a4316141480
mchingis <53611127+MChingS@users.noreply.github.com>
2021-08-13 17:09:53 +0200

    student

--- SchoolProject/src/com/company/Main.java
+++ SchoolProject/src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String [] args) {
+
+        StudentRegistration getStudentRegistration = new StudentRegistration();
+
+        getStudentRegistration.createFile();
+        getStudentRegistration.enteringStudentDetails();
     }
 }
--- SchoolProject/src/com/company/StudentRegistration.java
+++ SchoolProject/src/com/company/StudentRegistration.java
@@ -1,14 +1,133 @@
 package com.company;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 public class StudentRegistration {
 
-    public void studentDetails()
-    {
-        System.out.println("Enter your name");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+    boolean fileCondition=false;
+    String filename ="";
+    class student {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getIDNumber() {
+            return IDNumber;
+        }
+
+        public void setIDNumber(String IDNumber) {
+            this.IDNumber = IDNumber;
+        }
+
+        public String getEmailAddress() {
+            return emailAddress;
+        }
+
+        public void setEmailAddress(String emailAddress) {
+            this.emailAddress = emailAddress;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getCourse() {
+            return course;
+        }
+
+        public void setCourse(String course) {
+            this.course = course;
+        }
+
+        String name, surname, IDNumber;
+        String emailAddress, phoneNumber;
+        String address, course;
+
     }
+    public void enteringStudentDetails()
+    {
+        student toStudent = new student();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name :");
+        toStudent.name = input.nextLine();
+        System.out.print("Enter your surname :");
+        toStudent.surname = input.nextLine();
+        System.out.print("Enter your ID number :");
+        toStudent.IDNumber = input.nextLine();
+        System.out.print("Enter your phone number :");
+        toStudent.phoneNumber = input.nextLine();
+        System.out.print("Enter your email address :");
+        toStudent.emailAddress = input.nextLine();
+        System.out.print("Enter your course :");
+        toStudent.course = input.nextLine();
+        if(fileCondition==true) {
+            try {
+                FileWriter writer = new FileWriter(filename.toString()+".txt");
+                writer.write("Your name is :" +toStudent.name);
+                writer.write("\nYour surname is :"+toStudent.surname);
+                writer.write("\nYour ID number is :"+toStudent.IDNumber);
+                writer.write("\nYour phone number is :"+toStudent.phoneNumber);
+                writer.write("\nYour email address is :"+toStudent.emailAddress);
+                writer.write("\nYour course name is :"+toStudent.course);
+                writer.write("\n");
+                writer.write("\n DONE!!!!!!");
+                writer.write("\n");
+                writer.close();
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+            }
+        }
+        else
+        {
+            System.out.println("File was not created restart the program..");
+        }
+    }
+    public void createFile(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name to save file with :");
+        filename = input.nextLine();
+        try {
+            File myObj = new File(filename.toString()+".txt");
+            if (myObj.createNewFile())
+            {
+                System.out.println("File created: " + myObj.getName());
+                fileCondition=true;
+            } else {
+                System.out.println("File name already exists data in the file will be over written.");
+                fileCondition=true;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred. you inserted special characters on the file name");
+
+        }
+    }
+
 }
